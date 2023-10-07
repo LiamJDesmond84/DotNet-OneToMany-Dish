55cd77b4128afcb848a852e1fdbc9d00c7cfd3de
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-07 17:29:22 -0500

    Updating Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("ConnectionString");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 
 var app = builder.Build();
 
