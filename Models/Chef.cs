894eacc24eab05b52736a3b7006842e179acd7d5
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-07 17:37:40 -0500

    Updating models

--- Models/Chef.cs
+++ Models/Chef.cs
@@ -1,6 +1,19 @@
-﻿namespace DotNet_OneToMany_Dish.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_OneToMany_Dish.Models
 {
     public class Chef
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        public List<Dish> Dishes { get; set; }
     }
 }
--- Models/Dish.cs
+++ Models/Dish.cs
@@ -1,14 +1,20 @@
-﻿namespace DotNet_OneToMany_Dish.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_OneToMany_Dish.Models
 {
     public class Dish
     {
-
+        [Key]
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public int Calories { get; set; }
+
         public int Tastiness { get; set; }
+
         public String Description { get; set; }
 
-        public Chef Chef { get; set; }
+        public Chef ChefName { get; set; }
     }
 }
