c3234384225754c1892bcc5ebcfda97a48e9a0e7
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-07 17:40:24 -0500

    Added DbContext

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_OneToMany_Dish.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Chef> Chefs { get; set; }
+
+        public DbSet<Dish> Dishes { get; set; }
+    }
+}
