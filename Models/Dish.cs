5ebecc369e9e6953e9a990d22becbde3040f7cca
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-07 17:33:30 -0500

    Updating models

--- Models/Dish.cs
+++ Models/Dish.cs
@@ -2,5 +2,13 @@
 {
     public class Dish
     {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public int Tastiness { get; set; }
+        public String Description { get; set; }
+
+        public Chef Chef { get; set; }
     }
 }
