a758e7115ef31dce441a658cd1a4fce98f1b2b7e
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-15 11:56:13 -0500

    Updating view

---
+++ Migrations/20231015165211_whatever2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNet_OneToMany_Dish.Migrations
+{
+    /// <inheritdoc />
+    public partial class whatever2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Chefs",
+                newName: "ChefId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ChefId",
+                table: "Chefs",
+                newName: "Id");
+        }
+    }
+}
