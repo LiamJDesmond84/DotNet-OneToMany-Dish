9c9304574b1b7d1ba6c2419fb6122f7da187cffe
Liam James Desmond <liamjdesmond84@gmail.com>
2023-10-15 12:39:40 -0500

    Dropdownlist not sending value to controller

---
+++ Migrations/20231015173909_whatever4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNet_OneToMany_Dish.Migrations
+{
+    /// <inheritdoc />
+    public partial class whatever4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "selectedValue",
+                table: "Dishes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "selectedValue",
+                table: "Dishes",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
